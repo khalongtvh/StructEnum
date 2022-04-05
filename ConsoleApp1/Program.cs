078ce32d7956185c221518692b535155c2580164
Tran Khanh <63302419+khalongtvh@users.noreply.github.com>
2022-04-05 08:43:36 +0700

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -27,32 +27,46 @@ namespace ConsoleApp1
             SV.DiemTB_229 = ((int)diemMonHoc_229.toan_229 + (int)diemMonHoc_229.anh_229 + (int)diemMonHoc_229.van_229) /3;
         }
 
+        static void NhapSinhVien(SinhVien[] sv_229)
+        {
+            for (int i_229 = 0; i_229 < sv_229.Length; i_229++)
+            {
+                NhapThongTinSinhVien(out sv_229[i_229]);
+            }
+        }
         static void XuatThongTinSinhVien(SinhVien SV)
         {
             Console.WriteLine(" MSV : " + SV.MSV_229);
             Console.WriteLine(" Ho ten: " + SV.HoTen_229);
             Console.WriteLine(" Diem TB: " + SV.DiemTB_229);
         }
-
-        static void Main(string[] args)
+        static void XuatSinhVien(SinhVien[] sv_229)
         {
-            
-
-            SinhVien[] sv_229 = new SinhVien[3];
-            for (int i_229 = 0; i_229 < sv_229.Length; i_229++)
+            foreach (SinhVien item in sv_229)
             {
-                NhapThongTinSinhVien(out sv_229[i_229]);
+                XuatThongTinSinhVien(item);
             }
-            Console.Write("nhap ten sinh vien can tim : ");
-            string name_229 = Console.ReadLine();
-            
-            for (int i = 0; i< sv_229.Length; i++)
+        }
+
+        static void Tim_SV(SinhVien[] sv_229, string name_229)
+        {
+            for (int i = 0; i < sv_229.Length; i++)
             {
                 if (sv_229[i].HoTen_229.Equals(name_229))
                 {
                     Console.WriteLine("diem trung binh : " + sv_229[i].DiemTB_229);
                 }
             }
+
+        }
+        static void Main(string[] args)
+        {
+            SinhVien[] sv_229 = new SinhVien[3];
+            NhapSinhVien(sv_229);
+            XuatSinhVien(sv_229);
+            Console.Write("nhap ten sinh vien can tim : ");
+            string name_229 = Console.ReadLine();
+            Tim_SV(sv_229, name_229);
         }
     }
 }
